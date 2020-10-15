dd414e9fe2230efff118d714c9f1554923e197d1
tieuminhminh <nguyenleminh0805@gmail.com>
2020-10-15 14:32:34 +0700

    initialize moodledata

---
+++ lang/vi/auth_nntp.php
@@ -0,0 +1,31 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'auth_nntp', language 'vi', branch 'MOODLE_33_STABLE'
+ *
+ * @package   auth_nntp
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['auth_nntpdescription'] = 'Phương pháp này sử dụng một máy chủ NNTP để kiểm tra khi nào tên đăng nhập và mật khẩu đưa ra là hợp lệ.';
+$string['auth_nntphost'] = 'Các địa chỉ máy chủ NNTP. Sử dụng địa chỉ IP, không sử dụng tên DNS.';
+$string['auth_nntpport'] = 'Cổng máy chủ (119 là phổ biến nhất )';
+$string['pluginname'] = 'Sử dụng một máy chủ NNTP';
---
+++ lang/vi/block_tags.php
@@ -0,0 +1,80 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_tags', language 'vi', branch 'MOODLE_33_STABLE'
+ *
+ * @package   block_tags
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['add'] = 'Thêm';
+$string['alltags'] = 'Tất cả thẻ:';
+$string['arrowtitle'] = 'Nhấn vào đây để nhập ký tự gợi ý (các ký tự xám).';
+$string['configtitle'] = 'Tên khối';
+$string['coursetags'] = 'Các thẻ khóa học:';
+$string['defaultdisplay'] = 'Loại thẻ hiển thị';
+$string['disabledtags'] = 'Các thẻ bị vô hiệu hóa';
+$string['edit'] = 'sửa...';
+$string['editdeletemytag'] = 'Xóa thẻ khỏi khóa học này';
+$string['editmytags'] = 'Thẻ của tôi - lối tắt đến tất cả khóa học được gắn thẻ của bạn.';
+$string['editmytagsfor'] = 'Sửa thẻ của tôi đối với {$a}';
+$string['editnopersonaltags'] = 'Chưa có thẻ cá nhân được tạo.';
+$string['edittags'] = 'Sửa thẻ của tôi...';
+$string['edittagthisunit'] = 'Thêm thẻ vào khóa học này:';
+$string['editthiscoursetags'] = 'Các thẻ của tôi đối với khóa học này là {$a}';
+$string['edittitle'] = 'Thẻ của tôi';
+$string['entries'] = 'các mục';
+$string['entry'] = 'mục';
+$string['jserror1'] = 'Các thẻ phải có từ một đến 50 ký tự. Hãy điều chỉnh thẻ của bạn.';
+$string['jserror2'] = 'Thẻ không thể chứa các ký tự đặc biệt này - dấu xuyệt ngược, nhỏ hơn (<) hay lớn hơn (>). Hảy điều chỉnh thẻ của bạn.';
+$string['login'] = 'đăng nhập';
+$string['more'] = 'thêm...';
+$string['moreorder'] = 'Thứ tự:';
+$string['moreorderalpha'] = 'Theo bảng chữ cái';
+$string['moreorderdate'] = 'Ngày tạo';
+$string['moreorderpop'] = 'Độ phổ biến';
+$string['moreshow'] = 'Hiện:';
+$string['moreshowalltags'] = 'Tất cả thẻ';
+$string['moreshowcommtags'] = 'Các thẻ không chính thức';
+$string['moreshowcoursetags'] = 'Thẻ cho \'{$a}\'';
+$string['moreshowmytags'] = 'Thẻ của tôi';
+$string['moreshowofficialtags'] = 'Các thẻ chính thức';
+$string['moretags'] = 'Hiện và lọc thêm các thẻ';
+$string['moretitle'] = 'Thêm các thẻ';
+$string['morewelcome'] = 'Chào đón đám mây thẻ \'Thêm thẻ\'
+Các thẻ là các liên kết đến những thứ mà người dùng tạo. Thẻ cho phép bạn phân loại và liên kết những thứ
+như khóa học ưa thích, blog hay hồ sơ của bạn với những từ của riêng mình.
+Các nhóm thẻ khác nhau có thể được hiển thị với liên kết trên dòng \'Hiện:\',
+và có thể được xếp lại với các liên kết trên dòng \'Xếp:\'.';
+$string['mycoursetags'] = 'Thẻ khóa học của tôi:';
+$string['mytags'] = 'Thẻ của tôi:';
+$string['notagsyet'] = 'Chưa có thẻ';
+$string['please'] = 'Hãy';
+$string['pluginname'] = 'Thẻ';
+$string['select'] = 'Chọn..';
+$string['showcoursetags'] = 'Hiện các thẻ khóa học';
+$string['showcoursetagsdef'] = 'Hiển các tính năng gắn thẻ khóa học trong khối thẻ, cho phép học viên gắn thẻ khóa học.';
+$string['suggestedtagthisunit'] = 'Thẻ được đề xuất cho khóa học này:';
+$string['tags'] = 'thẻ';
+$string['tags:addinstance'] = 'Thêm khối thẻ';
+$string['tags:myaddinstance'] = 'Thêm khối thẻ vào Nhà của tôi';
+$string['tagthisunit'] = 'Gắn thẻ khóa học này:';
+$string['tagunits'] = 'để gắn thẻ khóa học ưa thích của bạn';
